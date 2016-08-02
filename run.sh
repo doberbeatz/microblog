d29ea7c09feb257673bfe94de938a455659afd46
Roman <doberbeatz@gmail.com>
2016-08-02 22:48:46 +0300

    Add copy .env to run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,11 @@ fi
 
 source ./flaskenv/bin/activate                      # Activate Env for this project
 ./flaskenv/bin/pip install -r requirements.txt
+
+# Copy .env file
+if [! -f ./.env ]; then
+    cp ./.env.example ./.env
+fi
 source ./.env
+
 ./run.py                                            # Run Python Web Server
